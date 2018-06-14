0d5702e83f64ded4a3526422dd64837f33047efb
Clipsey <TheisCHR@live.dk>
2018-06-14 20:37:21 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 /*
 	THIS IS A TEST HELLO WORLD PROGRAM, YOU CAN USE uwu transpile main to transpile it!
+	(Remember to compile this first via GCC or whatever compiler you prefer, lol)
 */
 #include <stdio.h>
 
